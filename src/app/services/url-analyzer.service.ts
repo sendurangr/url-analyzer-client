828648d94fd503fc56cce15729df3ce24c1ee260
Senduran <senduran40@gmail.com>
2025-04-30 07:23:24 +0530

    URL Changed

--- src/app/services/url-analyzer.service.ts
+++ src/app/services/url-analyzer.service.ts
@@ -9,12 +9,13 @@ export class UrlAnalyzerService {
 
   constructor(private http: HttpClient) { }
 
+  // backend : https://8pmmtnd3yw.ap-south-1.awsapprunner.com
+  // frontend : https://d2tiqwdij4sc1n.cloudfront.net
+  getStats(url: string): Observable<any> {
+    return this.http.get(`https://8pmmtnd3yw.ap-south-1.awsapprunner.com/url-analyzer?url=${url}`)
+  }
 
   // getStats(url: string): Observable<any> {
-  //   return this.http.get(`https://8pmmtnd3yw.ap-south-1.awsapprunner.com/url-analyzer?url=${url}`)
+  //   return this.http.get(`http://localhost:8080/url-analyzer?url=${url}`)
   // }
-
-  getStats(url: string): Observable<any> {
-    return this.http.get(`http://localhost:8080/url-analyzer?url=${url}`)
-  }
 }
