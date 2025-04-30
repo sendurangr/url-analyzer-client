6469867ed25d9d32684f60178cbfd663a756a2c5
Senduran <senduran40@gmail.com>
2025-04-30 07:30:17 +0530

    Angular Animations Removed for Successful Build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -17,7 +16,6 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   ],
   providers: [
     provideHttpClient(withFetch()),
-    provideAnimationsAsync(),
   ],
   bootstrap: [AppComponent]
 })
